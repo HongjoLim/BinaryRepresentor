be77ae16f9c22e437376ff9ce4c3c84c7a9d23d9
Hongjo Lim <hongjolim@geotab.com>
2021-10-01 21:03:39 -0700

    Implemented method to get number of bits to highlight for little endian and big endian

--- BinaryRepresentor/BinaryRepresentor/BitCalculator.cs
+++ BinaryRepresentor/BinaryRepresentor/BitCalculator.cs
@@ -66,5 +66,53 @@ namespace BinaryRepresentor
             Array.Copy(bits, bitOffset, bitsToChange, 0, valueSize);
             return bitsToChange;
         }
+
+        public static int GetNumOfBitsToHighlightBigEndian(int minIndex, int maxIndex, ushort valueSize, ushort bitOffset)
+        {
+            int numOfBitsToHighlight = 0;
+
+            if (minIndex > bitOffset)
+            {
+                numOfBitsToHighlight = 0;
+            }
+            else if (bitOffset <= maxIndex)
+            {
+                numOfBitsToHighlight = valueSize <= maxIndex - bitOffset + 1 ? valueSize : maxIndex - bitOffset + 1;
+            }
+            else
+            {
+                int numOfBytesHighlighted = (int)Math.Ceiling((bitOffset - maxIndex) / 8m) + 1;
+                int numOfBitsHighlighted = numOfBytesHighlighted * 8 - (bitOffset - maxIndex) - 7 + (numOfBytesHighlighted - 2) * 8;
+
+                numOfBitsToHighlight = valueSize >= numOfBitsHighlighted + 8 ? 8 : valueSize - numOfBitsHighlighted >= 0 ? valueSize - numOfBitsHighlighted : 0;
+            }
+
+            return numOfBitsToHighlight;
+        }
+
+        public static int GetNumOfBitsToHighlightLittleEndian(int minIndex, int maxIndex, ushort valueSize, ushort bitOffset)
+        {
+            int numOfBitsToHighlight = 0;
+
+            if (maxIndex < bitOffset || minIndex > bitOffset + valueSize)
+            {
+                numOfBitsToHighlight = 0;
+            }
+            else if (minIndex <= bitOffset && maxIndex >= bitOffset)
+            {
+                numOfBitsToHighlight = valueSize <= maxIndex + 1 - bitOffset ? valueSize : maxIndex + 1 - bitOffset;
+            }
+            else if (maxIndex < bitOffset + valueSize)
+            {
+                numOfBitsToHighlight = 8;
+            }
+            // The last bitset to highlight
+            else
+            {
+                numOfBitsToHighlight = (bitOffset + valueSize) % 8;
+            }
+
+            return numOfBitsToHighlight;
+        }
     }
 }

99e21fb77c9a8eab7e0a644ebe2738e43f9b08e6
Hongjo Lim <hongjolim@geotab.com>
2021-10-22 12:15:46 -0700

    Fixed comments

--- BinaryRepresentor/BinaryRepresentorTest/BitCalculatorTests/GetBitsToChangeFromDataTest.cs
+++ BinaryRepresentor/BinaryRepresentorTest/BitCalculatorTests/GetBitsToChangeFromDataTest.cs
@@ -8,7 +8,7 @@ namespace UniversalAutomotiveTester.Tests.OutputCalculatorTests
         [Fact]
         public void GetBitsToChangeFromData_WithLittleEndianTrue_WithBitOffset12_ValueSizeUpto8()
         {
-            // Assert
+            // Arrange
             byte[] data = new byte[8] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
             bool isLittleEndian = true;
             ushort valueSize = 8;
@@ -18,21 +18,13 @@ namespace UniversalAutomotiveTester.Tests.OutputCalculatorTests
             var result = BitCalculator.GetBitsToChangeFromData(data, isLittleEndian, bitOffset, valueSize);
 
             // Assert - 0x52 in bits
-            Assert.False(result[0]);
-            Assert.True(result[1]);
-            Assert.False(result[2]);
-            Assert.True(result[3]);
-
-            Assert.False(result[4]);
-            Assert.False(result[5]);
-            Assert.True(result[6]);
-            Assert.False(result[7]);
+            Assert.Equal(0x52, BitCalculator.BitArrayToLong(result));
         }
 
         [Fact]
         public void GetBitsToChangeFromData_WithLittleEndianTrue_WithBitOffset12_ValueSizeGreaterThan8()
         {
-            // Assert
+            // Arrange
             byte[] data = new byte[8] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
             bool isLittleEndian = true;
             ushort valueSize = 16;
@@ -42,32 +34,13 @@ namespace UniversalAutomotiveTester.Tests.OutputCalculatorTests
             var result = BitCalculator.GetBitsToChangeFromData(data, isLittleEndian, bitOffset, valueSize);
 
             // Assert - 0x7452 in bits - 0111 0100 0101 0010
-            Assert.Equal(16, result.Length);
-            Assert.False(result[0]);
-            Assert.True(result[1]);
-            Assert.True(result[2]);
-            Assert.True(result[3]);
-
-            Assert.False(result[4]);
-            Assert.True(result[5]);
-            Assert.False(result[6]);
-            Assert.False(result[7]);
-
-            Assert.False(result[8]);
-            Assert.True(result[9]);
-            Assert.False(result[10]);
-            Assert.True(result[11]);
-
-            Assert.False(result[12]);
-            Assert.False(result[13]);
-            Assert.True(result[14]);
-            Assert.False(result[15]);
+            Assert.Equal(0x7452, BitCalculator.BitArrayToLong(result));
         }
 
         [Fact]
         public void GetBitsToChangeFromData_WithLittleEndianFalse_WithBitOffset12_ValueSizeUpTo8()
         {
-            // Assert
+            // Arrange
             byte[] data = new byte[8] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
             bool isLittleEndian = false;
             ushort valueSize = 8;
@@ -77,21 +50,13 @@ namespace UniversalAutomotiveTester.Tests.OutputCalculatorTests
             var result = BitCalculator.GetBitsToChangeFromData(data, isLittleEndian, bitOffset, valueSize);
 
             // Assert - 0x12 in bits
-            Assert.False(result[0]);
-            Assert.False(result[1]);
-            Assert.False(result[2]);
-            Assert.True(result[3]);
-
-            Assert.False(result[4]);
-            Assert.False(result[5]);
-            Assert.True(result[6]);
-            Assert.False(result[7]);
+            Assert.Equal(0x12, BitCalculator.BitArrayToLong(result));
         }
 
         [Fact]
         public void GetBitsToChangeFromData_WithLittleEndianFalse_WithBitOffset20_ValueSizeGreaterThan8()
         {
-            // Assert
+            // Arrange
             byte[] data = new byte[8] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
             bool isLittleEndian = false;
             ushort valueSize = 16;
@@ -101,25 +66,7 @@ namespace UniversalAutomotiveTester.Tests.OutputCalculatorTests
             var result = BitCalculator.GetBitsToChangeFromData(data, isLittleEndian, bitOffset, valueSize);
 
             // Assert - 0x1234 in bits
-            Assert.False(result[0]);
-            Assert.False(result[1]);
-            Assert.False(result[2]);
-            Assert.True(result[3]);
-
-            Assert.False(result[4]);
-            Assert.False(result[5]);
-            Assert.True(result[6]);
-            Assert.False(result[7]);
-
-            Assert.False(result[8]);
-            Assert.False(result[9]);
-            Assert.True(result[10]);
-            Assert.True(result[11]);
-
-            Assert.False(result[12]);
-            Assert.True(result[13]);
-            Assert.False(result[14]);
-            Assert.False(result[15]);
+            Assert.Equal(0x1234, BitCalculator.BitArrayToLong(result));
         }
     }
 }
